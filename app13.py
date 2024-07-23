d0b82f4a34c7eb727f134572b4f26897f56e3f09
xiaoming <lininruc@gmail.com>
2024-07-23 23:35:02 +0800

    Ad mvs

---
+++ app13.py
@@ -0,0 +1,59 @@
+from playwright.sync_api import sync_playwright
+
+def extract_content(url, selector):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
+        content = page.locator(selector).all_text_contents()
+        browser.close()
+        return content
+
+def write_to_file(content):
+    if content_exists_in_file(content):
+        print("===={} has exists , so ignore.".format(content))
+        return
+    else:
+        with open('SearchEngineContent.txt', 'a+', encoding='utf-8') as file:
+            file.write(content + "\n")
+
+## 判断是否存在，如果存在就不用重复记录了.
+def content_exists_in_file(content):
+    try:
+        with open('SearchEngineContent.txt', 'r', encoding='utf-8') as file:
+            file_content = file.read()
+            return content in file_content
+    except FileNotFoundError:
+        # 如果文件不存在，返回False
+        return False
+
+def main(keyword):
+
+    url1 = "https://www.google.com.hk/search?q={}+site%3Aai&newwindow=1&sca_esv=b2c38071ce043d54&hl=zh-CN&sxsrf=ADLYWIL1F1I7QXuoER7U_zVoH71YXGGNNg%3A1721745257334&ei=ab-fZriIFKXc2roP6q2x6QI&ved=0ahUKEwj44NPzsL2HAxUlrlYBHepWLC0Q4dUDCA8&uact=5&oq=assistant+site%3Aai&gs_lp=Egxnd3Mtd2l6LXNlcnAiEWFzc2lzdGFudCBzaXRlOmFpSMaNAVDkBFiPiwFwEHgBkAEBmAGQBKABwjGqAQwwLjI2LjMuMC4xLjK4AQPIAQD4AQGYAgWgAvEHwgIKEAAYsAMY1gQYR8ICBBAjGCfCAgoQIxiABBgnGIoFwgIFEAAYgATCAhAQLhiABBjRAxhDGMcBGIoFwgILEC4YgAQY0QMYxwHCAhAQLhiABBjRAxjHARgKGMsBwgIIEAAYgAQYywGYAwCIBgGQBgeSBwUxLjEuM6AH9kw&sclient=gws-wiz-serp".format(keyword)
+    url2 = "https://www.google.com.hk/search?q={}+site:ai&newwindow=1&sca_esv=b2c38071ce043d54&hl=zh-CN&sxsrf=ADLYWIKIfxRwR-0R65VKSOWFeHifa_QSbg:1721745278035&ei=fr-fZq3lAdGq0-kP46Xo4AY&start=10&sa=N&sstk=Aagrsuj3BXnZeqPtlSqpK-dL9VzXFJOIjMMMW5xIAJmis7A9debXehf1xG8jsacHljR85Fwjb4R9upOgpU_8uXK5VhvaOG5jZ86OmQ&ved=2ahUKEwitnMP9sL2HAxVR1TQHHeMSGmwQ8tMDegQIDBAE&biw=1057&bih=883&dpr=2".format(keyword)
+
+    selector = '//*[@id="rso"]/div//div/div/div[1]/div/div/span/a/div/div/div/div[2]/cite'
+
+    content1 = extract_content(url1, selector)
+    content2 = extract_content(url2, selector)
+
+    for item in content1:
+        content = "{} , {}".format(item.split(" ")[0],keyword)
+        print(content)
+        write_to_file(content)
+
+    for item in content2:
+        content = "{} , {}".format(item.split(" ")[0],keyword)
+        print(content)
+        write_to_file(content)
+
+# main("assistant")
+# main("companion")
+
+# main("music")
+# main("ai generate")
+# main("ai write")
+# main("video")
+
+main("learning")
+main("conpanion")
---
+++ app14.py
@@ -0,0 +1,80 @@
+from playwright.sync_api import sync_playwright
+from dotenv import load_dotenv
+import os
+import logging
+
+# Load environment variables
+load_dotenv()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Headers for the request
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    'cookie': os.environ.get("COOKIE"),
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+}
+
+def fetch_website_data(page, keyword):
+    url = f"https://pro.similarweb.com/#/digitalsuite/websiteanalysis/overview/website-performance/*/999/3m?webSource=Total&key={keyword}"
+    xpath = '//*[@id="react-app"]/div/div[4]/div[2]/div[2]/span/div/div/span/div[3]/div[2]/section/div/section/div[2]/div/div[1]/div[3]/div[1]/div[3]'
+    xpath_rate = '//*[@id="react-app"]/div/div[4]/div[2]/div[2]/span/div/div/span/div[3]/div[2]/section/div/section/div[2]/div/div[1]/div[3]/div[1]/div[4]/div[1]/div/span[2]'
+
+    try:
+        page.set_extra_http_headers(headers)
+        page.goto(url)
+        page.wait_for_timeout(5000)  # 等待页面加载
+        content = page.locator(xpath).text_content()
+        rate = page.locator(xpath_rate).text_content()
+        print(f"{keyword} == {content} == {rate}")
+        write_to_file(f"{keyword} == {content} == {rate}",keyword)
+    except Exception as e:
+        logging.error(f"ERROR for {keyword}: {e}")
+
+def write_to_file(content,keyword):
+    if content_exists_in_file(keyword):
+        print("===={} has exists , so ignore.".format(keyword))
+        return
+    else:
+        with open('SimillarwebContent.txt', 'a+', encoding='utf-8') as file:
+            file.write(content + "\n")
+
+## 判断是否存在，如果存在就不用重复记录了.
+def content_exists_in_file(content):
+    try:
+        with open('SimillarwebContent.txt', 'r', encoding='utf-8') as file:
+            file_content = file.read()
+            return content in file_content
+    except FileNotFoundError:
+        # 如果文件不存在，返回False
+        return False
+    
+def main(keywords):
+    with sync_playwright() as p:
+        browser = p.webkit.launch()
+        page = browser.new_page()
+        for keyword in keywords:
+            fetch_website_data(page, keyword)
+        browser.close()
+
+if __name__ == "__main__":
+    keywords = [
+        "perplexity.ai", 
+        "scite.ai",
+        "shadcn.com",
+        "moonshot.cn" 
+        
+        ]
+    main(keywords)
